2d463511d726facfe87242f58fbaaa27ef29a29f
oriong2@gmail.com <oriong2@gmail.com@c5d49534-a369-79f6-5e22-801cfaeca7b1>
2013-04-16 11:57:53 +0000

    Selected and estimation done

--- trunk/PF/ParticleFilter.m
+++ trunk/PF/ParticleFilter.m
@@ -1,7 +1,41 @@
-function [ output_args ] = ParticleFilter( N, z )
+function [ xk ] = ParticleFilter( N, z, particlesSize )
 %PARTICLEFILTER Summary of this function goes here
 %   Detailed explanation goes here
 
+
+files = dir(['./SecuenciaPF/kk/' '*.' 'jpeg']);
+frames_names = sort({files.name});
+num_frames = size( sort({files.name}),2);
+
+actual_frame = imread (strcat('./SecuenciaPF/kk/',frames_names{1}));
+[HEIGHT WIDTH C]= size(actual_frame);
+
+
+xk = randn(2, N);
+minxk = min(min(xk));
+xk = xk - minxk;
+
+maxxk = max(max(xk));
+xk = xk/maxxk;
+
+xk(1,:) = xk(1,:) * WIDTH;
+xk(2,:) = xk(2,:) * HEIGHT;
+
+xk = round(xk);
+
+for tt = 1:num_frames
+ 
+  actual_frame = imread (strcat('./SecuenciaPF/kk/',frames_names{tt}));
+  ball_frame = actual_frame(:,:,1);
+  ball_frame = im2bw(ball_frame, 175./255);
+     
+  imshow(ball_frame);
+
+  [wk, wkidx] = evaluacion(xk, particlesSize, ball_frame);
+  sumwk = sum(wk);
+  wk = wk/sumwk;
+  
+  xk = estimacion(xk, wkidx);
 
 end
 
--- trunk/PF/seleccion.m
+++ trunk/PF/seleccion.m
@@ -1,7 +1,32 @@
-function [ output_args ] = seleccion( input_args )
+function [xk, wk] = seleccion( xk, wk )
 %SELECCION Summary of this function goes here
 %   Detailed explanation goes here
+N = length(wk);
 
+rullete = rand(N,1);
+
+
+for j = 1:N
+    rullValue = rullete(j);
+    wAcum = 0;
+    kk(j) = 0;
+    
+    for k = 1:N
+        wAcum = wAcum + wk(k);
+        if(wAcum < rullValue)
+            kk(j) = xk(k);
+        else
+            break;
+        end
+    end;
+    
+    
+end
+
+%Inicialización de una población de partículas
+for i = 1:N
+   w(i) = 1/N;
+end
 
 end
 
