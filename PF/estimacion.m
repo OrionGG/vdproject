4b167cd36f240eafb931c7525ec2737b49fa3c59
oriong2@gmail.com <oriong2@gmail.com@c5d49534-a369-79f6-5e22-801cfaeca7b1>
2013-04-16 11:57:53 +0000

    Selected and estimation done

--- PF/estimacion.m
+++ PF/estimacion.m
@@ -1,7 +1,12 @@
-function [ output_args ] = estimacion( input_args )
+function [ xkO ] = estimacion( xkI, wkidx )
 %ESTIMACION Summary of this function goes here
 %   Detailed explanation goes here
+N = length(wkidx);
 
+for k = 1:N
+    xkO(1,k) = xkI(1,wkidx(k));
+    xkO(2,k) = xkI(2,wkidx(k));
+end
 
 end
 
--- PF/evaluacion.m
+++ PF/evaluacion.m
@@ -1,7 +1,37 @@
-function [ output_args ] = evaluacion( input_args )
+function [ wk, wkidx ] = evaluacion( xk, particleSize, ball_frame )
 %EVALUACION Summary of this function goes here
 %   Detailed explanation goes here
+N = size(xk,2);
+[HEIGHT WIDTH C]= size(ball_frame);
 
+l = 1;
+
+for k = 1:N
+    acumValues = 0;
+    x0 = round(xk(1,k) - particleSize./2);
+    y0 = round(xk(2,k) - particleSize./2);
+    xf = round(xk(1,k) + particleSize./2);
+    yf = round(xk(2,k) + particleSize./2);
+    
+    if(x0 < 1) x0 = 1; end;
+    if(y0 < 1) y0 = 1; end;
+    if(xf > WIDTH) xf = WIDTH; end;
+    if(yf > HEIGHT) yf = HEIGHT; end;
+    
+    
+    
+    for i = y0:yf
+        for j = x0:xf     
+            acumValues = acumValues + ball_frame(i,j);
+        end
+    end
+    
+    wk(k) = acumValues;
+    if (wk(k)> 0)
+        wkidx(l) = k;
+        l = l+1;
+    end 
+end
 
 end
 
